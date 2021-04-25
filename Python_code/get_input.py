f38ed9e6f4a69dd5154713a6d2160b1fc6a955ff
Syntodev <Syntodev@github.com>
2021-04-25 14:33:04 +0200

    added first partial support for xml-Tags

--- Python_code/get_input.py
+++ Python_code/get_input.py
@@ -22,10 +22,39 @@ def get_text_from_xml(abs_paths_to_xml_data):
         all_text.append(current_text[0].text)
 
     return all_text
-    #return current_text[0].text
-#my_input = "test"
-#my_input = get_input_data()
-#print(my_input)
+
+def get_tags_from_xml(abs_paths_to_xml_data):
+
+    sub_tag_tags = set("")
+    read_all = 1
+    print(abs_paths_to_xml_data)
+    for xml_file in abs_paths_to_xml_data:
+        tree = ElementTree.parse(xml_file)
+        root = tree.getroot()
+        for elem in root:
+            if elem.tag == 'TAGS':
+                for sub_tag in elem:
+                    #print(sub_tag.tag)
+                    sub_tag_tags.add(sub_tag.tag)
+                #print(elem.tag)
+            #for subelem in elem:
+                #print("HERE IT COMES")
+                #print(subelem,":")
+                #print(subelem.attrib)
+        if read_all == 0:
+            break
+    print(sub_tag_tags)
+    '''
+
+    all_tags = []
+
+    for xml_document in abs_paths_to_xml_data:
+        dom = ElementTree.parse(xml_document)
+        current_text = dom.findall('PLACE')
+        all_tags.append(current_text[0].text)
+    '''
+    all_tags = []
+    return all_tags
 
 def get_abs_path_to_xml_files():
     cur_abs_path = os.path.abspath("")
@@ -41,8 +70,12 @@ def get_abs_path_to_xml_files():
 def get_input_data():
     abs_paths_to_xml_files = get_abs_path_to_xml_files()
     all_text_in_lists = get_text_from_xml(abs_paths_to_xml_files)
+    all_tags_in_lists = get_tags_from_xml(abs_paths_to_xml_files)
     return all_text_in_lists
 
+#print(get_input_data())
+if __name__ == '__main__':
+    get_input_data()
 #ret = get_input_data()
 #for line in ret:
     #print(line)
--- Python_code/main.py
+++ Python_code/main.py
@@ -1,29 +1,66 @@
 import spacy
 import get_input as gi
+import csv
+import os
 #spacy.cli.download("en_core_web_sm")   #<---------- ggf muss das hier laufen, falls es bisher nicht installiert ist.
+
 
 nlp = spacy.load("en_core_web_sm")
 
 
-input = gi.get_input_data()
+def create_dir_for_saving_data(new_directory_name):
+    show_warnings = 0
+    show_info = 0
+    cur_abs_path = os.path.abspath("")
+    path = os.path.join(cur_abs_path, new_directory_name)
+    if show_info == 1:
+        print("Wir versuchen den Ordner '"+str(new_directory_name)+"' zu erstellen um dort die von Spacy tokenisierten und"
+              " mit PoS versehenen Texte zu speichern.")
+    try:
+        os.mkdir(path)
+        print("Wir haben einen neuen Order mit dem Pfad:",path,"erstellt.")
+    except FileExistsError:
+        if show_warnings == 1:
+            print("Der Ordner '"+str(new_directory_name)+"' existiert bereits, wir werden ihn nicht erneut erstellen.")
+    except:
+        print("Ein unvorhergesehener Fehler ist aufgetreten.")
+        print("GGF. stimmt etwas mit dem Pfad für die Ordnererstellung nicht.")
 
 
+def write_into_csv_file(new_directory_name, output_csv_filename, all_texts):
+    cur_abs_path = os.path.abspath("")
+    path = os.path.join(cur_abs_path, new_directory_name)
+    os.chdir(path)
+    full_output_csv_name = str(output_csv_filename) + ".csv"
+    with open(full_output_csv_name, 'w', newline='') as myfile:
+        thewriter = csv.writer(myfile)
+        thewriter.writerow(['Wort', 'PoS', 'Satzende?'])
 
-def test_2(text):
-    doc = nlp(text)
+        for text in all_texts:
+            doc = nlp(text)
+            for sentence in doc.sents:
+                #print("Ein Satz beginnt")
+                counter = 0
+                words_in_sentence = len(sentence)
+                for token in sentence:
+                    counter += 1
+                    if counter != words_in_sentence:
+                        thewriter.writerow([token.text, token.pos_])
+                    else:
+                        thewriter.writerow([token.text, token.pos_, "Satzende"])
 
-    for token in doc:
-        print(token.text, token.pos_)
-    #for ent in doc.ents:
-        #print(ent.text, ent.label_)
-        #print(ent.text, ent.label_)
 
-    #print(doc.text)
-    print(spacy.explain("GPE"))
+def main():
+    all_texts = gi.get_input_data()
+    new_directory_name = "output_data"
+    output_csv_filename = "output_data"
+
+    create_dir_for_saving_data(new_directory_name)
+    #write_into_csv_file(new_directory_name, output_csv_filename, all_texts)
+    write_into_csv_file(new_directory_name, output_csv_filename,all_texts[0:1])
 
 
-test_2(input[0])
-
+main()
 
 #for text_passage in input:
     #test_2(text_passage)
@@ -34,3 +71,30 @@ test_2(input[0])
 #TODO #TODO #TODO #TODO #TODO #TODO #TODO #TODO
 #Was können wir bisher:
 #Wir können den Text von allen .xml Dateien lesen und diese mit mithilfe von Spacy mit PoS Tags versehen.
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+def create_csv_file(new_directory_name, output_csv_filename):
+    cur_abs_path = os.path.abspath("")
+    path = os.path.join(cur_abs_path, new_directory_name)
+    os.chdir(path)
+    full_output_csv_name = str(output_csv_filename)+".csv"
+    with open(full_output_csv_name, 'w', newline='') as myfile:
+        column_names = ['col1', 'col2', 'col3']
+        thewriter = csv.writer(myfile)
+
+        thewriter.writerow(["aaaaaa", "hmm"])
+
+
+'''
